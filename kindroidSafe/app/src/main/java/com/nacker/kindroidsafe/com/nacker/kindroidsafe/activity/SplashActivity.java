1d1d557c96d1a55aa5ac1f67870c57dc58506ad0
zhen.lei <zhen.lei@kindroid.com>
2016-09-01 16:44:17 +0800

    合并代码

--- kindroidSafe/app/src/main/java/com/nacker/kindroidsafe/com/nacker/kindroidsafe/activity/SplashActivity.java
+++ kindroidSafe/app/src/main/java/com/nacker/kindroidsafe/com/nacker/kindroidsafe/activity/SplashActivity.java
@@ -2,14 +2,18 @@ package com.nacker.kindroidsafe.com.nacker.kindroidsafe.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
 
 import com.nacker.kindroidsafe.R;
 
 public class SplashActivity extends AppCompatActivity {
 
+    //requestWindowFeature(Window.FEATURE_NO_TITLE); 去除当前Activity头Title
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_splash);
     }
 }
