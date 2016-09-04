d9802970f08ba796cc32b6377826299d4b176d51
zhen.lei <zhen.lei@kindroid.com>
2016-09-05 00:13:04 +0800

    Toast

--- kindroidSafe/app/src/main/java/com/nacker/kindroidsafe/utils/ToastUtil.java
+++ kindroidSafe/app/src/main/java/com/nacker/kindroidsafe/utils/ToastUtil.java
@@ -1,6 +1,7 @@
 package com.nacker.kindroidsafe.utils;
 
 import android.content.Context;
+import android.widget.Toast;
 
 /**
  * Created by nacker on 16/9/2.
@@ -12,7 +13,7 @@ public class ToastUtil {
      * @param msg    打印文本内容
      */
     public static void show(Context ctx, String msg) {
-//        Toast.makeText(ctx,msg,0).show();
+        Toast.makeText(ctx,msg,0).show();
     }
 
 
